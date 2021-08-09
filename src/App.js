f907d52186cdbc360918b96ae1215afa61a00af6
Laurie Lim Sam <laurie.limsam@gmail.com>
2021-08-09 14:47:12 +0300

    Add link to repo and copyright

--- src/App.js
+++ src/App.js
@@ -73,7 +73,16 @@ function App() {
 					deleteHandler={deleteHandler}
 				/>
 			</main>
-			<footer></footer>
+			<footer>
+				<div>
+					<a href='//github.com/laurielim/REACT21K_REACT_JS_TODO_APP'>
+						Github Repo
+					</a>
+				</div>
+				<div>
+					2021 © <a href='//laurielim.github.io/'>Laurie Lim Sam</a>
+				</div>
+			</footer>
 		</div>
 	);
 }
